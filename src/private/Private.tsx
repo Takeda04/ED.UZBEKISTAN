1f4b2b7abf1d8deb3c9e1ca08faec497d7be60ba
bahtibek-an <bahtibekanvarov@gmail.com>
2023-10-13 14:21:31 +0500

    change private component

--- src/private/Private.tsx
+++ src/private/Private.tsx
@@ -4,13 +4,12 @@ import { Navigate } from "react-router-dom";
 const Private: React.FC<{ children: React.JSX.Element }> = ({ children }) => {
   const ACCESS_TOKEN: string | null = localStorage.getItem("accessToken");
   const userRole = localStorage.getItem("userRole");
-  return children;
-  // if (ACCESS_TOKEN && ACCESS_TOKEN?.length > 900) {
-  //   // if(userRole === 'student'){
-  //   //   return 
-  //   // }
-  //     return children;
-  // }
+  if (ACCESS_TOKEN && ACCESS_TOKEN?.length > 900) {
+    // if(userRole === 'student'){
+    //   return 
+    // }
+      return children;
+  }
 
   // If there's no access token, navigate to the login page
   return <Navigate to="/login" replace />;
