4a28538bcaff9c3be14ea48c84f5209948598636
olimjon <olimjon@MacBook-Pro-olimjon.local>
2023-10-13 15:30:45 +0500

    clas

---
+++ src/components/Cards/ClassCard.tsx
@@ -0,0 +1,9 @@
+const ClassCard = (props: any) => {
+  return (
+    <main className=" max-w-6xl w-80 max-h-48 h-40 flex items-center justify-center bg-white rounded-xl">
+            sinflar
+    </main>
+  )
+}
+
+export default ClassCard
--- src/pages/Classes.tsx
+++ src/pages/Classes.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { v4 } from "uuid"
+import ClassCard from "../components/Cards/ClassCard"
 
 const Classes = () => {
+
+  const classes = [{ class_num: 1 }, { class_num: 2 }, { class_num: 3 }, { class_num: 4 }, { class_num: 5 }, { class_num: 6 }, { class_num: 7 }, { class_num: 8 }, { class_num: 9 },{ class_num: 10 },{ class_num: 11 } ]
+  
   return (
-    <div>Classes</div>
+    <main className='h-screen flex flex-wrap items-center justify-center overflow-y-scroll '>
+      <div className="grid grid-cols-3 gap-4 my-1">
+        {classes?.map(({ class_num}) =>(
+          <ClassCard class={class_num} key={v4()} />
+        ))}
+        </div>
+    </main>
   )
 }
 
