5f99dfa5a8dd36fcb1b5f639b1497169db646b86
bahtibek-an <bahtibekanvarov@gmail.com>
2023-10-13 14:20:49 +0500

    add tailwind css

--- src/private/Private.tsx
+++ src/private/Private.tsx
@@ -4,12 +4,13 @@ import { Navigate } from "react-router-dom";
 const Private: React.FC<{ children: React.JSX.Element }> = ({ children }) => {
   const ACCESS_TOKEN: string | null = localStorage.getItem("accessToken");
   const userRole = localStorage.getItem("userRole");
-  if (ACCESS_TOKEN && ACCESS_TOKEN?.length > 900) {
-    // if(userRole === 'student'){
-    //   return 
-    // }
-      return children;
-  }
+  return children;
+  // if (ACCESS_TOKEN && ACCESS_TOKEN?.length > 900) {
+  //   // if(userRole === 'student'){
+  //   //   return 
+  //   // }
+  //     return children;
+  // }
 
   // If there's no access token, navigate to the login page
   return <Navigate to="/login" replace />;
