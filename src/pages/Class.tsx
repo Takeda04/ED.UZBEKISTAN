8587d665d508755028fe3de168143171a6e1ba02
olimjon <olimjon@MacBook-Pro-olimjon.local>
2023-10-14 19:20:50 +0500

    class-map

--- src/pages/Class.tsx
+++ src/pages/Class.tsx
@@ -5,7 +5,7 @@ import SinfCard from "../components/Cards/SinfCard";
 
 const ClassComponent = () => {
   const { num } = useParams();
-  const sinf = classes.find((item) => {
+  const sinf: { class_num: number, id: number, categories: string[] } | undefined = classes.find((item) => {
     if (!num) return false;
     return item.id === +num;
   });
@@ -13,7 +13,7 @@ const ClassComponent = () => {
   return (
     <main className="flex flex-wrap items-center justify-center mt-2">
       <Box className="grid grid-cols-1 sm:grid-cols-2 w-[900]:grid-cols-3 xl:grid-cols-3 gap-4 my-1">
-        {sinf?.categories?.((item) => (
+        {sinf?.categories?.map((item) => (
           <SinfCard item={item} sinf_raqam={sinf.class_num} />
         ))}
       </Box>
