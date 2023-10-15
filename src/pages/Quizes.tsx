c9613ce755ad38c08ee058da2f0785d150053581
olimjon <olimjon@MacBook-Pro-olimjon.local>
2023-10-15 08:59:24 +0500

    random

--- src/pages/Quizes.tsx
+++ src/pages/Quizes.tsx
@@ -1,19 +1,36 @@
-import { Box } from "@mui/material";
-import InputSelect from "../components/Cards/InputSelect";
-import { quizes } from "../static/data";
-import { Container } from "../static/tags";
+import React, { useEffect, useState } from 'react';
+import { Box } from '@mui/material';
+import InputSelect from '../components/Cards/InputSelect';
+import { Container } from '../static/tags';
+import { AI } from '../static/AI';
+
+// Define a type for the AI questions
+type Question = {
+  question: string;
+  options: { [key: string]: string };
+  true_answer: string;
+  description: string;
+  video: string;
+};
 
 const Quizes = () => {
+  // Explicitly define the type for randomQuestions
+  const [randomQuestions, setRandomQuestions] = useState<Question[]>([]);
+
+  useEffect(() => {
+    // AI dan tasodifiy 25 ta savolni tanlash
+    const shuffledAI = AI.sort(() => 0.5 - Math.random());
+    const selectedQuestions = shuffledAI.slice(0, 25);
+
+    setRandomQuestions(selectedQuestions);
+  }, []);
+
   return (
     <Container>
       <Box className="flex flex-col gap-y-5">
-        {quizes?.map(({ question, options, correctAnswer }) => (
-          <Box className="border-b border-black py-3">
-            <InputSelect
-              key={correctAnswer}
-              question={question}
-              options={options}
-            />
+        {randomQuestions.map(({ question, options, true_answer }) => (
+          <Box className="border-b border-black py-3" key={true_answer}>
+            <InputSelect question={question} options={options} />
           </Box>
         ))}
       </Box>
