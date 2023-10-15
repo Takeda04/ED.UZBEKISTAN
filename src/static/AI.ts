631a2c3742aa44621cada13cbec7738aeb4e1b0e
olimjon <olimjon@MacBook-Pro-olimjon.local>
2023-10-15 13:02:25 +0500

    ful

--- src/static/AI.ts
+++ src/static/AI.ts
@@ -55,172 +55,169 @@ export const AI = [
   "video": "https://www.youtube.com/watch?v=bBHomOA-FHs&list=PLGO4HdV8zhh-gepadWs2Li_ANkwnaA-Bs&index=4"
 }
 ,
+
   {
-    question: "5. 9 ning darajasi = ?",
-    options: {
-      a: "18",
-      b: "81",
-      c: "27",
-      d: "64",
-      f: "bilmayman",
+    "question": "1 dan 10 gacha bo'lgan sonlarni sanang.",
+    "options": {
+      "a": "20",
+      "b": "15",
+      "c": "45",
+      "d": "10",
+      "f": "bilmayman"
     },
-    true_answer: "b",
-    description: "Ushbu savol 9 ni kvadratini qanday hisoblash mumkin?",
-    video: "https://www.youtube.com/watch?v=0nk5o4tlPqg",
+    "true_answer": "d",
+    "description": "Ushbu savol 1 dan 10 gacha bo'lgan sonlarni sanash uchun. Natija nechta bo'ladi?",
+    "video": "https://www.youtube.com/watch?v=PefaGjYKda8&list=PLGO4HdV8zhh-gepadWs2Li_ANkwnaA-Bs&index=5"
+  }
+  ,
+  {
+    "question": "1/4 va 3/5 kesirlarini qo'shing. Natija qanday kesir bo'ladi?",
+    "options": {
+      "a": "1/9",
+      "b": "11/20",
+      "c": "2/9",
+      "d": "17/20",
+      "f": "bilmayman"
+    },
+    "true_answer": "d",
+    "description": "Ushbu matematik savoli 1/4 va 3/5 kesirlarini qo'shishni talab qiladi. Natija 17/20 bo'ladi.",
+    "video": "https://www.youtube.com/watch?v=usNipv3XHmw"
+  }
+  ,
+  {
+    "question": "2/3 va 3/4 kesirlarni ko'paytiring. Natija qanday kesir bo'ladi?",
+    "options": {
+      "a": "1/12",
+      "b": "5/7",
+      "c": "2/3",
+      "d": "9/12",
+      "f": "bilmayman"
+    },
+    "true_answer": "c",
+    "description": "Ushbu matematik savoli 2/3 va 3/4 kesirlarni ko'paytirishni talab qiladi. Natija 2/3 bo'ladi.",
+    "video": "https://www.youtube.com/watch?v=usNipv3XHmw"
+  }
+  ,
+  {
+    "question": "2/5 va 1/3 kesirlarni qo'shing. Natija qanday kesir bo'ladi?",
+    "options": {
+      "a": "1/6",
+      "b": "2/15",
+      "c": "3/8",
+      "d": "7/15",
+      "f": "bilmayman"
+    },
+    "true_answer": "d",
+    "description": "Ushbu matematik savoli 2/5 va 1/3 kesirlarni qo'shishni talab qiladi. Natija 7/15 bo'ladi.",
+    "video": "https://www.youtube.com/watch?v=usNipv3XHmw"
+  }
+  ,
+  {
+    "question": "1 dan 10 gacha bo'lgan kare sonlar qaysilari?",
+    "options": {
+      "a": "3, 5, 7, 9",
+      "b": "1, 2, 3, 4, 5, 6, 7, 8, 9, 10",
+      "c": "2, 4, 6, 8, 10",
+      "d": "4, 9, 16, 25, 36, 49, 64, 81, 100",
+      "f": "bilmayman"
+    },
+    "true_answer": "a",
+    "description": "Ushbu matematik savoli 1 dan 10 gacha bo'lgan kare sonlarni topishni talab qiladi. Kare sonlar 1, 4, 9 va 16 sonlari.",
+    "video": "https://www.youtube.com/watch?v=4_n4teZwhdI&t=1s"
+  }
+  ,
+  {
+    "question": "64 sonining kvadrat ildizini toping.",
+    "options": {
+      "a": "4",
+      "b": "8",
+      "c": "16",
+      "d": "32",
+      "f": "bilmayman"
+    },
+    "true_answer": "b",
+    "description": "Ushbu matematik savol 64 sonining kvadrat ildizini topishni talab qiladi. 64 sonining kvadrat ildizi 8 ga teng.",
+    "video": "https://www.youtube.com/watch?v=-5NdueU5ZPk"
+  }
+  ,
+  
+    {
+      "question": "4 ning kvadrat ildizini toping.",
+      "options": {
+        "a": "2",
+        "b": "8",
+        "c": "16",
+        "d": "64",
+        "f": "bilmayman"
+      },
+      "true_answer": "a",
+      "description": "Ushbu matematik savoli 4 ning kvadrat ildizini topishni talab qiladi. 4 ning kvadrat ildizi 2 ga teng.",
+    "video": "https://www.youtube.com/watch?v=-5NdueU5ZPk",
   },
   {
-    question: "6. 7 ning yarmi qancha?",
-    options: {
-      a: "3.5",
-      b: "3",
-      c: "4",
-      d: "2.5",
-      f: "bilmayman",
+    "question": "Diskriminant nima?",
+    "options": {
+      "a": "Bu kvadrat tenglamani yechish uchun ishlatiladigan ko'rsatkich.",
+      "b": "Kubik tenglamalarning yechimi uchun ishlatiladigan o'zgaruvchisiz ko'rsatkich.",
+      "c": "Sonlarni yig'ish uchun ishlatiladigan o'zgaruvchisiz ko'rsatkich.",
+      "d": "Kvadrat tenglamani yechish uchun ishlatiladigan o'zgaruvchili ko'rsatkich.",
+      "f": "bilmayman"
     },
-    true_answer: "a",
-    description:
-      "Bu savolning javobi 7 ni ikki ga bo'lib topish orqali aniqlanadi. 7 ning yarmi 3.5 ga teng.",
-    video: "https://www.youtube.com/watch?v=4_n4teZwhdI",
+    "true_answer": "d",
+    "description": "Diskriminant kvadrat tenglamalarni yechishda ishlatiladi va x^2, x va ya o'zgaruvchisiz a va b ko'rsatkichlari orasidagi ayirmani topish uchun foydalaniladi.",
+    "video" : "https://www.youtube.com/watch?v=-5NdueU5ZPk",
   },
   {
-    question: "7. 17 - 5 = ?",
-    options: {
-      a: "12",
-      b: "10",
-      c: "7",
-      d: "8",
-      f: "bilmayman",
+    "question": "Diskriminantni qanday hisoblash mumkin?",
+    "options": {
+      "a": "D = b^2 - 4ac",
+      "b": "D = 2ab - c",
+      "c": "D = 3ac + b^2",
+      "d": "D = a^2 + 4bc",
+      "f": "bilmayman"
     },
-    true_answer: "b",
-    description:
-      "Bu savol o'rtacha ayirish amalini testlaydi. 17 dan 5 ni ayrib chiqish natijasi 10 ga teng.",
-    video: "https://www.youtube.com/watch?v=VHM3zYa3Le8",
+    "true_answer": "a",
+    "description": "Diskriminantni hisoblash formulasi D = b^2 - 4ac. Bu formuladan foydalanib, kvadrat tenglamalar uchun o'zgaruvchisiz ayirmani hisoblash mumkin.",
+    "video": "https://www.youtube.com/watch?v=-5NdueU5ZPk",
   },
   {
-    question: "8. 13 ning tub bo'luvidan qanday qoldi?",
-    options: {
-      a: "6",
-      b: "7",
-      c: "5",
-      d: "8",
-      f: "bilmayman",
+    "question": "Diskriminantni qanday foydalanish mumkin?",
+    "options": {
+      "a": "Kvadrat tenglamalarning yechimini hisoblash uchun.",
+      "b": "O'zgaruvchisiz chiziqlar yaratish uchun.",
+      "c": "O'zgaruvchisiz tenglama yechimini hisoblash uchun.",
+      "d": "Kubik tenglamalarni yechish uchun.",
+      "f": "bilmayman"
     },
-    true_answer: "b",
-    description:
-      "Bu savol 13 ni tub bo'luvchi sonlarga bo'lib tekshiradi. 13 ning tub bo'luvi 1 va 13. Ular orasidagi kichik tub son 7.",
-    video: "https://www.youtube.com/watch?v=zOFK0hz_tpk",
+    "true_answer": "a",
+    "description": "Diskriminant, kvadrat tenglamalarning yechimini hisoblashda ishlatiladi. Diskriminantning qiymati kvadrat tenglamalarning yechimining haqiqiy ildizlarining sonini bildiradi va ularning qaysi turda bo'lishi aniqlaydi.",
+    "video": "https://www.youtube.com/watch?v=-5NdueU5ZPk",
   },
   {
-    question: "9. 16 ning juft sonlar ko'paytmasi qancha?",
-    options: {
-      a: "32",
-      b: "64",
-      c: "16",
-      d: "8",
-      f: "bilmayman",
+    "question": "Diskriminantning qiymati qanday tafsilotli?",
+    "options": {
+      "a": "Qaror qilib olish uchun foydalaniladi.",
+      "b": "Kvadrat tenglamalarning ildizlarini bildiradi.",
+      "c": "Bu bir son va o'zgaruvchi uchun berilgan qiymatni hisoblash uchun ishlatiladi.",
+      "d": "Kubning hajmi haqida ma'lumot beradi.",
+      "f": "bilmayman"
     },
-    true_answer: "d",
-    description:
-      "Bu savol 16 ning juft sonlar ko'paytmasini topishni tekshiradi. 16 ning juft sonlar ko'paytmasi 2 ga teng.",
-    video: "https://www.youtube.com/watch?v=ilRpuiCKY_s",
+    "true_answer": "b",
+    "description": "Diskriminantning qiymati kvadrat tenglamalarning ildizlarini bildiradi. Uning qiymati ko'pgina o'zgaruvchisiz kvadrat tenglamalarda qaysi turda haqiqiy ildizlar borligini va ularning qaysi turda yo'q ekanligini ko'rsatadi.",
+    "video": "https://www.youtube.com/watch?v=-5NdueU5ZPk",
   },
   {
-    question: "10. 9 ning kasr soni qanday?",
-    options: {
-      a: "0.1",
-      b: "1",
-      c: "0.9",
-      d: "0.01",
-      f: "bilmayman",
+    "question": "Diskriminantni qanday aniqlash mumkin?",
+    "options": {
+      "a": "Kubik tenglamalarni hisoblashda ishlatiladi.",
+      "b": "O'zgaruvchisiz tenglamalarni hisoblashda ishlatiladi.",
+      "c": "Kvadrat tenglamalarni yechishda ishlatiladi.",
+      "d": "Kvadrat tenglamalarni yechimini aniqlashda ishlatiladi.",
+      "f": "bilmayman"
     },
-    true_answer: "c",
-    description:
-      "Bu savol 9 ning kasr sonini topishni testlaydi. 9 ning kasr soni 0.9 ga teng.",
-    video: "https://www.youtube.com/watch?v=Km3TFe6Y2Us",
-  },
-  {
-    question: "11. Mukammal sonlar nimalar?",
-    options: {
-      a: "Yarim sonlar",
-      b: "Yegaki sonlar",
-      c: "To'liq bo'luvchi sonlar",
-      d: "Juft sonlar",
-      f: "bilmayman",
-    },
-    true_answer: "c",
-    description:
-      "Mukammal sonlar, o'zi va o'zi har bir bo'luvchilari yig'indisi o'zi ga teng bo'lgan sonlar. Masalan, 28 mukammal son, chunki uning bo'luvchilari 1, 2, 4, 7, 14, va 28 sonlari.",
-    video: "https://www.youtube.com/watch?v=84oLeMPtlqk",
-  },
-  {
-    question: "12. Sonli va harfli ifodalar nimalar?",
-    options: {
-      a: "Raqam va to'liq so'zlar",
-      b: "Yolg'iz so'zlar",
-      c: "Bir xil ifodalar",
-      d: "Juft sonlar",
-      f: "bilmayman",
-    },
-    true_answer: "a",
-    description:
-      "Sonli va harfli ifodalar, raqam va to'liq so'zlar qo'shildi va ularning jamlanmasi. Masalan, '123' sonli ifoda va 'hello' so'zi sonli va harfli ifodalardir.",
-    video: "https://www.youtube.com/watch?v=bCSTHBIdg6M",
-  },
-  {
-    question: "13. Qoldiqli bo‘lish nima?",
-    options: {
-      a: "Sonlarni ko'paytirish",
-      b: "Sonlarni bo'lib chiqish",
-      c: "Sonlarni qo'shish",
-      d: "Juft sonlar",
-      f: "bilmayman",
-    },
-    true_answer: "b",
-    description:
-      "Qoldiqli bo‘lish, ikki son orasida bo'lib chiqish amalini ifodalaydi. Masalan, 7 ni 3 ga bo'lib chiqishda qoldiq 1 bo'lishi mumkin.",
-    video: "https://www.youtube.com/watch?v=GlafUwEwXXo",
-  },
-  {
-    question: "14. Ulushlar nima?",
-    options: {
-      a: "Sonlarni ko'paytirish",
-      b: "Harfli so'zlar",
-      c: "To'liq sonlar",
-      d: "Juft sonlar",
-      f: "bilmayman",
-    },
-    true_answer: "b",
-    description:
-      "Ulushlar, harfli so'zlar va ularning ko'paytirilgan jamlanmasi. Masalan, 'apple' va 'banana' so'zlar ulushni tashkil etadi.",
-    video: "https://www.youtube.com/watch?v=YQJTN8cfPPY",
-  },
-  {
-    question: "15. Oddiy kasrlar haqida tushuncha nima?",
-    options: {
-      a: "Uluchmas so'zlar",
-      b: "Jumlah so'zlar",
-      c: "Nisbatan katta sonlar",
-      d: "Uzluksiz ko'paytirilgan so'zlar",
-      f: "bilmayman",
-    },
-    true_answer: "a",
-    description:
-      "Oddiy kasrlar, uluchmas so'zlar yoki uluchmas jamlanmalardir. Uluchmas, sifat va kofe, uchun misollar bo'lishi mumkin.",
-    video: "https://www.youtube.com/watch?v=YU5jtZHnNjM",
-  },
-  {
-    question: "16. Kasrlarni taqqoslash nima?",
-    options: {
-      a: "Kasrlarni yig'ish",
-      b: "Kasrlarni ko'paytirish",
-      c: "Kasrlarni ulushlash",
-      d: "Kasrlarni taqqoslash",
-      f: "bilmayman",
-    },
-    true_answer: "d",
-    description:
-      "Kasrlarni taqqoslash, ularni ulg'aytirish, jamlanmasini o'stirish, ya'ni kattalashtirishni ifodalaydi. Masalan, 1/2 + 1/3 = 5/6.",
-    video: "https://www.youtube.com/watch?v=YU5jtZHnNjM",
+    "true_answer": "d",
+    "description": "Diskriminant, kvadrat tenglamalarni yechishda va ularning haqiqiy ildizlarini aniqlashda ishlatiladi. Kvadrat tenglamalardan olingan D qiymati kvadrat tenglamalarning yechimining haqiqiy ildizlarining sonini aks ettiradi.",
+    "video": "https://www.youtube.com/watch?v=-5NdueU5ZPk",
   },
   {
     question: "17. To‘g‘ri va noto‘g‘ri kasrlar nimalar?",
