9d54a2ed16333ae64e79d4ffda3575155370ae88
olimjon <olimjon@MacBook-Pro-olimjon.local>
2023-10-15 11:19:44 +0500

    all

--- src/components/Cards/HomeCard.tsx
+++ src/components/Cards/HomeCard.tsx
@@ -1,6 +1,6 @@
 import { Box, Grid } from "@mui/material";
 
-const HomeCard = () => {
+const HomeCard:React.FC<{url: string, description: string, level: string}> = ({url, description, level}) => {
 
 
     return (
@@ -9,7 +9,7 @@ const HomeCard = () => {
                 <Box>
                     <Box
                         className="rounded-t-lg"
-                        src="https://fs.getcourse.ru/fileservice/file/download/a/564257/sc/307/h/899975eccd05dce8ab1d8f1f010e87d6.png"
+                        src={url}
                         component="img"
                         sx={{
                             height: 180,
@@ -21,15 +21,13 @@ const HomeCard = () => {
                 <Box className="p-4">
                     <Box className="font-medium mt-2">
                         Level: 
-                        <span className="font-normal ml-1">Intermediate</span>
+                        <span className="font-normal ml-1">{level}</span>
                     </Box>
                     <Box
                         className="mt-2 text-sm"
                         component="p"
                     >
-                        At this level, the student will be able to understand and communicate in familiar situations, 
-                        use a range of tenses and complex sentence structures, comprehend spoken and written English, 
-                        and express themselves with a degree of fluency and accuracy.
+                        {description}
                     </Box>
                 </Box>
             </Box>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,25 +1,47 @@
-import React from 'react'
-import { Container, Title } from '../static/tags'
-import HomeCard from '../components/Cards/HomeCard'
-import { Grid } from '@mui/material'
+import { Container, Title } from "../static/tags";
+import HomeCard from "../components/Cards/HomeCard";
+import { Grid } from "@mui/material";
+import { v4 } from "uuid";
 
 const Main = () => {
+
+  const data = [
+    {
+      level: "Intermediate",
+      description:
+        "At this level, the student will be able to understand and communicate in familiar situations, use a range of tenses and complex sentence structures, comprehend spoken and written English, and express themselves with a degree of fluency and accuracy.",
+      url: "https://fs.getcourse.ru/fileservice/file/download/a/564257/sc/307/h/899975eccd05dce8ab1d8f1f010e87d6.png",
+    },
+    {
+      level: "Intermediate",
+      description:
+        "At this level, the student will be able to understand and communicate in familiar situations, use a range of tenses and complex sentence structures, comprehend spoken and written English, and express themselves with a degree of fluency and accuracy.",
+      url: "https://fs.getcourse.ru/fileservice/file/download/a/564257/sc/307/h/899975eccd05dce8ab1d8f1f010e87d6.png",
+    },
+    {
+      level: "Intermediate",
+      description:
+        "At this level, the student will be able to understand and communicate in familiar situations, use a range of tenses and complex sentence structures, comprehend spoken and written English, and express themselves with a degree of fluency and accuracy.",
+      url: "https://fs.getcourse.ru/fileservice/file/download/a/564257/sc/307/h/899975eccd05dce8ab1d8f1f010e87d6.png",
+    },
+    {
+      level: "Intermediate",
+      description:
+        "At this level, the student will be able to understand and communicate in familiar situations, use a range of tenses and complex sentence structures, comprehend spoken and written English, and express themselves with a degree of fluency and accuracy.",
+      url: "https://fs.getcourse.ru/fileservice/file/download/a/564257/sc/307/h/899975eccd05dce8ab1d8f1f010e87d6.png",
+    },
+  ];
   return (
     <Container>
-      <Title className='border-b border-black mt-4'>
-        My courses
-      </Title>
+      <Title className="border-b border-black mt-4">My courses</Title>
 
-      <Grid className="!mt-2"  container spacing={2}>
-        <HomeCard/>
-        <HomeCard/>
-        <HomeCard/>
-        <HomeCard/>
+      <Grid className="!mt-2" container spacing={2}>
+        {data?.map(({url, level, description}) => (
+        <HomeCard key={v4()} url={url} level={level} description={description} />
+        ))}
       </Grid>
-      
-      
     </Container>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
