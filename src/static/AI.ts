644c5852f75185639485de8f74cabc4f20205736
olimjon <olimjon@MacBook-Pro-olimjon.local>
2023-10-15 08:33:17 +0500

    AI

---
+++ src/static/AI.ts
@@ -0,0 +1,262 @@
+export const AI = [
+    {
+        question: "1. 3 + 4 = ?",
+        options: {
+            a: "5",
+            b: "6",
+            c: "7",
+            d: "8",
+            f: "bilmayman"
+        },
+        true_answer: "c",
+        description: "Ushbu savol 3 va 4 ni qo'shib nechta hosil qiladi?",
+        video: "https://www.youtube.com/watch?v=VHM3zYa3Le8"
+    },
+    {
+        question: "2. 8 * 5 = ?",
+        options: {
+            a: "10",
+            b: "20",
+            c: "30",
+            d: "40",
+            f: "bilmayman"
+        },
+        true_answer: "d",
+        description: "Ushbu savol 8 ni 5 marta ko'paytirganda nechta hosil qiladi?",
+        video: "https://www.youtube.com/watch?v=4_n4teZwhdI"
+    },
+    {
+        question: "3. 12 / 2 = ?",
+        options: {
+            a: "2",
+            b: "4",
+            c: "6",
+            d: "8",
+            f: "bilmayman"
+        },
+        true_answer: "c",
+        description: "Ushbu savol 12 ni 2 ga bo'lib qanday natija hosil qiladi?",
+        video: "https://www.youtube.com/watch?v=4_n4teZwhdI"
+    },
+    {
+        question: "4. 15 - 7 = ?",
+        options: {
+            a: "5",
+            b: "6",
+            c: "7",
+            d: "8",
+            f: "bilmayman"
+        },
+        true_answer: "d",
+        description: "Ushbu savol 15 dan 7 ni ayrib qancha hosil qiladi?",
+        video: "https://www.youtube.com/watch?v=VHM3zYa3Le8"
+    },
+    {
+        question: "5. 9 ning darajasi = ?",
+        options: {
+            a: "18",
+            b: "81",
+            c: "27",
+            d: "64",
+            f: "bilmayman"
+        },
+        true_answer: "b",
+        description: "Ushbu savol 9 ni kvadratini qanday hisoblash mumkin?",
+        video: "https://www.youtube.com/watch?v=0nk5o4tlPqg"
+    },
+    {
+        question: "6. 7 ning yarmi qancha?",
+        options: {
+            a: "3.5",
+            b: "3",
+            c: "4",
+            d: "2.5",
+            f: "bilmayman"
+        },
+        true_answer: "a",
+        description: "Bu savolning javobi 7 ni ikki ga bo'lib topish orqali aniqlanadi. 7 ning yarmi 3.5 ga teng.",
+        video: "https://www.youtube.com/watch?v=4_n4teZwhdI"
+    },
+    {
+        question: "7. 17 - 5 = ?",
+        options: {
+            a: "12",
+            b: "10",
+            c: "7",
+            d: "8",
+            f: "bilmayman"
+        },
+        true_answer: "b",
+        description: "Bu savol o'rtacha ayirish amalini testlaydi. 17 dan 5 ni ayrib chiqish natijasi 10 ga teng.",
+        video: "https://www.youtube.com/watch?v=VHM3zYa3Le8"
+    },
+    {
+        question: "8. 13 ning tub bo'luvidan qanday qoldi?",
+        options: {
+            a: "6",
+            b: "7",
+            c: "5",
+            d: "8",
+            f: "bilmayman"
+        },
+        true_answer: "b",
+        description: "Bu savol 13 ni tub bo'luvchi sonlarga bo'lib tekshiradi. 13 ning tub bo'luvi 1 va 13. Ular orasidagi kichik tub son 7.",
+        video: "https://www.youtube.com/watch?v=zOFK0hz_tpk"
+    },
+    {
+        question: "9. 16 ning juft sonlar ko'paytmasi qancha?",
+        options: {
+            a: "32",
+            b: "64",
+            c: "16",
+            d: "8",
+            f: "bilmayman"
+        },
+        true_answer: "d",
+        description: "Bu savol 16 ning juft sonlar ko'paytmasini topishni tekshiradi. 16 ning juft sonlar ko'paytmasi 2 ga teng.",
+        video: "https://www.youtube.com/watch?v=ilRpuiCKY_s"
+    },
+    {
+        question: "10. 9 ning kasr soni qanday?",
+        options: {
+            a: "0.1",
+            b: "1",
+            c: "0.9",
+            d: "0.01",
+            f: "bilmayman"
+        },
+        true_answer: "c",
+        description: "Bu savol 9 ning kasr sonini topishni testlaydi. 9 ning kasr soni 0.9 ga teng.",
+        video: "https://www.youtube.com/watch?v=Km3TFe6Y2Us"
+    },
+    {
+        question: "11. Mukammal sonlar nimalar?",
+        options: {
+            a: "Yarim sonlar",
+            b: "Yegaki sonlar",
+            c: "To'liq bo'luvchi sonlar",
+            d: "Juft sonlar",
+            f: "bilmayman"
+        },
+        true_answer: "c",
+        description: "Mukammal sonlar, o'zi va o'zi har bir bo'luvchilari yig'indisi o'zi ga teng bo'lgan sonlar. Masalan, 28 mukammal son, chunki uning bo'luvchilari 1, 2, 4, 7, 14, va 28 sonlari.",
+        video: "https://www.youtube.com/watch?v=84oLeMPtlqk"
+    },
+    {
+        question: "12. Sonli va harfli ifodalar nimalar?",
+        options: {
+            a: "Raqam va to'liq so'zlar",
+            b: "Yolg'iz so'zlar",
+            c: "Bir xil ifodalar",
+            d: "Juft sonlar",
+            f: "bilmayman"
+        },
+        true_answer: "a",
+        description: "Sonli va harfli ifodalar, raqam va to'liq so'zlar qo'shildi va ularning jamlanmasi. Masalan, '123' sonli ifoda va 'hello' so'zi sonli va harfli ifodalardir.",
+        video: "https://www.youtube.com/watch?v=bCSTHBIdg6M"
+    },
+    {
+        question: "13. Qoldiqli bo‘lish nima?",
+        options: {
+            a: "Sonlarni ko'paytirish",
+            b: "Sonlarni bo'lib chiqish",
+            c: "Sonlarni qo'shish",
+            d: "Juft sonlar",
+            f: "bilmayman"
+        },
+        true_answer: "b",
+        description: "Qoldiqli bo‘lish, ikki son orasida bo'lib chiqish amalini ifodalaydi. Masalan, 7 ni 3 ga bo'lib chiqishda qoldiq 1 bo'lishi mumkin.",
+        video: "https://www.youtube.com/watch?v=GlafUwEwXXo"
+    },
+    {
+        question: "14. Ulushlar nima?",
+        options: {
+            a: "Sonlarni ko'paytirish",
+            b: "Harfli so'zlar",
+            c: "To'liq sonlar",
+            d: "Juft sonlar",
+            f: "bilmayman"
+        },
+        true_answer: "b",
+        description: "Ulushlar, harfli so'zlar va ularning ko'paytirilgan jamlanmasi. Masalan, 'apple' va 'banana' so'zlar ulushni tashkil etadi.",
+        video: "https://www.youtube.com/watch?v=YQJTN8cfPPY"
+    },
+    {
+        question: "15. Oddiy kasrlar haqida tushuncha nima?",
+        options: {
+            a: "Uluchmas so'zlar",
+            b: "Jumlah so'zlar",
+            c: "Nisbatan katta sonlar",
+            d: "Uzluksiz ko'paytirilgan so'zlar",
+            f: "bilmayman"
+        },
+        true_answer: "a",
+        description: "Oddiy kasrlar, uluchmas so'zlar yoki uluchmas jamlanmalardir. Uluchmas, sifat va kofe, uchun misollar bo'lishi mumkin.",
+        video: "https://www.youtube.com/watch?v=YU5jtZHnNjM"
+    },
+    {
+        question: "16. Kasrlarni taqqoslash nima?",
+        options: {
+            a: "Kasrlarni yig'ish",
+            b: "Kasrlarni ko'paytirish",
+            c: "Kasrlarni ulushlash",
+            d: "Kasrlarni taqqoslash",
+            f: "bilmayman"
+        },
+        true_answer: "d",
+        description: "Kasrlarni taqqoslash, ularni ulg'aytirish, jamlanmasini o'stirish, ya'ni kattalashtirishni ifodalaydi. Masalan, 1/2 + 1/3 = 5/6.",
+        video: "https://www.youtube.com/watch?v=YU5jtZHnNjM"
+    },
+    {
+        question: "17. To‘g‘ri va noto‘g‘ri kasrlar nimalar?",
+        options: {
+            a: "Sonlar",
+            b: "Yarim sonlar",
+            c: "Sifatlar",
+            d: "Kasrlar",
+            f: "bilmayman"
+        },
+        true_answer: "d",
+        description: "To‘g‘ri kasrlar, sonlar jamlanmasini yig'indisi o'zi va o'zi bo'luvchilari o'zi ga teng bo'lgan kasrlardir. Noto‘g‘ri kasrlar esa bu shartni bajaradigan barcha kasrlardir.",
+        video: "https://www.youtube.com/watch?v=zd2aWpONeR0"
+    },
+    {
+        question: "18. Bir xil maxrajli kasrlarni qo‘shish va ayirish nima?",
+        options: {
+            a: "Kasrlarni ko'paytirish",
+            b: "Kasrlarni qo'shish",
+            c: "Kasrlarni ayirish",
+            d: "Kasrlarni ulushlash",
+            f: "bilmayman"
+        },
+        true_answer: "c",
+        description: "Bir xil maxrajli kasrlarni qo‘shish va ayirish, ularni o'zaro bir xil maxrajga keltirishni ifodalaydi. Masalan, 1/4 + 1/4 = 1/2.",
+        video: "https://www.youtube.com/watch?v=xnciVhBIYUQ"
+    },
+    {
+        question: "19. Bo‘lish va kasrlar nima alohida?",
+        options: {
+            a: "Uluchmaslar",
+            b: "To‘g‘ri kasrlar",
+            c: "Aralash sonlar",
+            d: "Juft sonlar",
+            f: "bilmayman"
+        },
+        true_answer: "c",
+        description: "Bo‘lish, sonlar orasidagi kasrlarni hisoblash amalini ifodalaydi. Kasrlar, butun sonlar bo'ylab o'rnatilganlar bo'lib, bo‘lish, ko'paytirish, qo‘shish va ayirish amallarini bajarishda foydalaniladi.",
+        video: "https://www.youtube.com/watch?v=A1TYQtOvwtE"
+    },
+    {
+        question: "20. Aralash sonlar nimalar?",
+        options: {
+            a: "Sonlar va harfli so'zlar",
+            b: "Juft sonlar",
+            c: "Uluchmaslar",
+            d: "Ixtiyoriy sonlar",
+            f: "bilmayman"
+        },
+        true_answer: "d",
+        description: "Aralash sonlar, butun sonlar va kasrlar orasidagi sonlar bo'lib, ularni ko'paytirish va qo'shish amallarini ifodalaydi. Masalan, 3/4 + 2 = 11/4.",
+        video: "https://www.youtube.com/watch?v=ikP_Kt81tVA"
+    }
+]
